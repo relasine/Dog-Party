75e5b4a3dbca7a72ce1c6d8e89925952b8a957e2
Intrepid Panda <jerick999@gmail.com>
2019-01-20 14:30:08 -0700

    Updates

---
+++ precache-manifest.fc3b372a288377c73e0d974bb0086c4b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a554acfe9d2fde7814",
+    "url": "/Dog-Party/static/js/runtime~main.12a554ac.js"
+  },
+  {
+    "revision": "6dadb7fd78635c127619",
+    "url": "/Dog-Party/static/js/main.6dadb7fd.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/Dog-Party/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "6dadb7fd78635c127619",
+    "url": "/Dog-Party/static/css/main.34358621.chunk.css"
+  },
+  {
+    "revision": "5b8b40cc9b67b3e7545385530c28530b",
+    "url": "/Dog-Party/index.html"
+  }
+];
