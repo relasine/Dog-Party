c919f017852c3aeefde712f39c7afc0ffd4c10a0
Intrepid Panda <jerick999@gmail.com>
2019-01-20 14:51:55 -0700

    Create array of social link objects

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import './Footer.css';
 
+import twitter from '../../assets/twitter.svg';
+import instagram from '../../assets/instagram.svg';
+import facebook from '../../assets/facebook.svg';
+import linkedin from '../../assets/linkedin.svg';
+
 const Footer = () => {
+  const socialLinks = [
+  {icon: twitter},
+  {icon: instagram},
+  {icon: facebook},
+  {icon: linkedin}
+  ];
+
   return (
     <h3>Learn About Dogs Online</h3>
   )
