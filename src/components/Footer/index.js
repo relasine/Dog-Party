15ed39c1ef63d66e1eed3b444e21bcd70db3062a
Intrepid Panda <jerick999@gmail.com>
2019-01-20 15:19:31 -0700

    Add name strings to link objects, map over link array to product link anchors

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -8,11 +8,20 @@ import linkedin from '../../assets/linkedin.svg';
 
 const Footer = () => {
   const linkData = [
-  {icon: twitter, link: "https://twitter.com/search?q=dogs&src=typd"},
-  {icon: instagram, link: "https://www.instagram.com/dogsofinstagram/"},
-  {icon: facebook, link: "https://www.facebook.com/BlackDogAnimalRescue/"},
-  {icon: linkedin, link: "https://www.linkedin.com/company/support-dogs/"}
+  {name: 'Twitter', icon: twitter, link: "https://twitter.com/search?q=dogs&src=typd"},
+  {name: 'Instagram', icon: instagram, link: "https://www.instagram.com/dogsofinstagram/"},
+  {name: 'Facebook', icon: facebook, link: "https://www.facebook.com/BlackDogAnimalRescue/"},
+  {name: 'LinkedIn', icon: linkedin, link: "https://www.linkedin.com/company/support-dogs/"}
   ];
+
+  const socialLinks = linkData.map(link => {
+    return (
+      <a href={link.link} target='_blank'>
+        <img src={link.icon} alt=`${link.name} logo` />
+      </a>
+    )
+
+  });
 
   return (
     <h3>Learn About Dogs Online</h3>
