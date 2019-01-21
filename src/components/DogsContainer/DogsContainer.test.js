0dce73fa74fcabf8e449dd292ed1bbdd8cd2e1e4
Intrepid Panda <jerick999@gmail.com>
2019-01-20 18:43:14 -0700

    Create conditional to filter dogs displayed based on category

--- src/components/DogsContainer/DogsContainer.test.js
+++ src/components/DogsContainer/DogsContainer.test.js
@@ -5,12 +5,17 @@ import { shallow } from 'enzyme';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<DogsContainer dogs={[]}/>, div);
+  ReactDOM.render(<DogsContainer 
+                    dogs={[]}
+                    category={''} />,
+                  div);
   ReactDOM.unmountComponentAtNode(div);
 });
 
 it('should match the snapshot', () => {
-  const wrapper = shallow(<DogsContainer dogs={[]}/>);
+  const wrapper = shallow(<DogsContainer 
+                            dogs={[]} 
+                            category={''}/>);
 
   expect(wrapper).toMatchSnapshot();
 });
--- src/components/DogsContainer/index.js
+++ src/components/DogsContainer/index.js
@@ -3,13 +3,31 @@ import PropTypes from 'prop-types';
 import './DogsContainer.css';
 
 const DogsContainer = (props) => {
+  let dogs;
+
+  if (props.category === '') {
+    dogs = props.dogs.map(dog => {
+      return (
+        <article></article>
+      )
+    });
+  } else {
+    const theDog = props.dogs.find(dog => {
+      return props.category === dog.title;
+    });
+
+    dogs = <article></article>;
+  }
   return (
-    <section></section>
+    <section>
+      {dogs}
+    </section>
   )
 }
 
 DogsContainer.propTypes = {
-  dogs: PropTypes.array.isRequired
+  dogs: PropTypes.array.isRequired,
+  category: PropTypes.string.isRequired
 };
 
 export default DogsContainer;
