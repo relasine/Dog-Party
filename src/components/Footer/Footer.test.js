841aa9cf993cf9ae877c8f27b23fa122daa4cb61
Intrepid Panda <jerick999@gmail.com>
2019-01-20 17:51:10 -0700

    Fix pathing for test imports for footer and app

--- src/components/Footer/Footer.test.js
+++ src/components/Footer/Footer.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Footer from './Footer';
+import Footer from './index.js';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
