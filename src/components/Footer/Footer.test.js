714952323ce4175b328c8bfd8a07ab582a557f34
Intrepid Panda <jerick999@gmail.com>
2019-01-20 18:01:28 -0700

    Write and pass snapshot test

--- src/components/Footer/Footer.test.js
+++ src/components/Footer/Footer.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Footer from './index.js';
+import { shallow } from 'enzyme';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Footer />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('should match the snapshot', () => {
+  const wrapper = shallow(<Footer />);
+
+  expect(wrapper).toMatchSnapshot();
 });
